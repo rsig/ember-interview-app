04b822535003dcd8a52852c3d6abe3072b625fab
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2016-01-22 10:16:46 -0500

    upgrade ember and deps

--- app/controllers/products/edit.js
+++ app/controllers/products/edit.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    // What are some improvements that could be made to `update`?
     update(model) {
       model.save();
       this.transitionToRoute('products');
